81a9dbc6bbd9e162e3002a4312a74c0ec60790d6
Vishal Rawat <94183896+VishalRawatt@users.noreply.github.com>
2024-01-08 02:11:22 +0530

    backend completed

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,7 @@ const url = "mongodb+srv://Vishal:vishal@vishalrawat.m0jxh57.mongodb.net/Netflix
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
 
 mongoose.connect(url)
 .then(()=>{
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use("/api/auth",authRoute) ;
 app.use("/api/users",userRoute) ;
 app.use("/api/movies",movieRoute) ;
+app.use("/api/lists",listRoute) ; 
 
 app.listen(8080, ()  => {
     console.log('Serving running on port 8080');
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -5,7 +5,6 @@ const jwt = require("jsonwebtoken");
 require('dotenv').config() ;
 const secretKey = process.env.SECRET_KEY  ;
 
-
 //Register
 router.post("/register",  async(req, res) =>{
     const  newuser = new User({
@@ -30,7 +29,7 @@ router.post("/login", async(req,res)=>{
           res.status(401).json("Wrong password") ;
         }
 
-        const accessToken = jwt.sign({id: user._id, isAdmin: user.isAdmin},secretKey,{expiresIn:"1d"});
+        const accessToken = jwt.sign({id: user._id, isAdmin: user.isAdmin},secretKey,{expiresIn:"30d"});
 
         const { password, ...info } = user._doc ;
 
