08be0ffad52c410e8bf33f6062b5599f74e03fb8
Vishal Rawat <94183896+VishalRawatt@users.noreply.github.com>
2024-02-14 21:46:42 +0530

    Admin topside created

--- Admin/src/components/topbar/Topbar.jsx
+++ Admin/src/components/topbar/Topbar.jsx
@@ -22,7 +22,7 @@ export default function Topbar() {
                 <Notifications/>
                 <span className="topIconBag">3</span>
             </div>
-            
+            <img src="https://imgs.search.brave.com/Id5fz4T8jtqRaA1a9UrVJWgK6IVpqlHJV5JKKsPtI58/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by90ZWFtLWRpdmVy/c2UtcGVvcGxlLWRv/aW5nLWdyb3VwLXBo/b3RvXzUzODc2LTEz/ODQyMy5qcGc_c2l6/ZT02MjYmZXh0PWpw/Zw" alt="imageofpeople" className="topAvatar" />
         </div>
       </div>
     </div>
